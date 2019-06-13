cd553cc1f9bd6bb3314e27ea2a80f41098f2d8e2
Marcelo Contreras <marcelojoc@gmail.com>
2019-06-13 09:53:38 -0300

    descargar  archivos

---
+++ descargar_info.sh
@@ -0,0 +1,6 @@
+# ! /bin/bash
+# Programa para ejemplificar el uso de la descarga de información desde internet utilizando el comando wget
+# Autor: marcelo contreras
+
+echo "Descargar información de internet"
+wget https://eeo.eaoe.cc/fa89ff46066acbd1887fd1400867ff6f/wFPPunMQbdU
