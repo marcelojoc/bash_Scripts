3bb6cf0f0ebde91e7d24a8feccb1f92445bb1810
Marcelo Contreras <marcelojoc@gmail.com>
2019-07-13 16:02:06 -0300

    leer archivos

---
+++ leerArchivos.sh
@@ -0,0 +1,17 @@
+# ! /bin/bash
+# Programa para ejemplificar la lectura  de archivos
+# Autor: marcelo contreras
+
+
+echo "Leer en un archivo"
+cat $1
+echo -e "\nAlmacenar los valores en una variable"
+valorCat=`cat $1`
+echo "$valorCat"
+
+# Se utiliza la variable IFS (Internal Field Separator) para evitar que los espacios en blanco al inicio al final se recortan
+echo -e "\nLeer archivos línea por línea utilizando while"
+while IFS= read linea
+do
+    echo "$linea"
+done < $1 
