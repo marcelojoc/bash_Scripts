f4d06c749494283769424a9538b1b6d1cf4012d1
Marcelo Contreras <marcelojoc@gmail.com>
2019-07-13 09:48:35 -0300

    breack  continue

---
+++ breack_continue.sh
@@ -0,0 +1,20 @@
+# ! /bin/bash
+# Programa para ejemplificar breack  and  continue
+# Autor: marcelo contreras
+
+
+
+echo "Sentencias break y continue"
+for fil in $(ls)
+do
+    for nombre in {1..4}
+    do
+        if [ $fil = "10_download.sh" ]; then
+            break;
+        elif [[ $fil == 4* ]]; then
+            continue;
+        else
+            echo "Nombre archivo:$fil _ $nombre"
+        fi
+    done
+done
