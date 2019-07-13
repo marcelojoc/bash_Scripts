160373d98d577657a727242b08bb1770197edae1
Marcelo Contreras <marcelojoc@gmail.com>
2019-07-13 13:14:14 -0300

    crear archivos y directorios

---
+++ crearArchivosDir.sh
@@ -0,0 +1,17 @@
+# ! /bin/bash
+# Programa para ejemplificar la creacion de archivos  y directorios
+# Autor: marcelo contreras
+
+echo "Archivos  y directorios"
+
+if [ $1 = "d" ]; then
+    mkdir -m 755 $2
+    echo "Directorio creado correctamente"
+    ls -la $2
+elif [ $1 == "f" ]; then
+    touch $2
+    echo "Archivo creado correctamente"
+    ls -la $2
+else
+    echo "No existe esa opción: $1"
+ fi
