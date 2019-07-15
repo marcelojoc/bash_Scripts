93fbd419dde8f43473305844e6e8827499f406b8
Marcelo Contreras <marcelojoc@gmail.com>
2019-07-14 21:58:39 -0300

    Empaquetamiento TAR, GZIP y PBZIP 2

---
+++ empaqueTar.sh
@@ -0,0 +1,6 @@
+# ! /bin/bash
+# Programa para ejemplificar empaquetar  archivos  TAR
+# Autor: marcelo contreras
+
+echo "Empaquetar todos los scripts de la carpeta shellCourse"
+tar -cvf shellCourse.tar *.sh
---
+++ tar_gzip.sh
@@ -0,0 +1,12 @@
+# ! /bin/bash
+# Programa para ejemplificar empaquetar  archivos  GZIP
+# Autor: marcelo contreras
+
+echo "Empaquetar todos los scripts de la carpeta shellCourse"
+tar -cvf shellCourse.tar *.sh
+
+# Cuando se empaqueta con gzip el empaquetamiento anterior se elimina
+gzip shellCourse.tar
+
+echo "Empaquetar un solo archivo, con un ratio 9"
+gzip -9 9_options.sh 
